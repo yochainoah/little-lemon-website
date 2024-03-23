52c4badd2275378367ae78cc31bf20d6067d1bb9
yochainoah <yochyochai123@gmail.com>
2024-03-23 19:50:59 +0200

    building the css for the website

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,10 @@
+import './App.css';
 import { ReactComponent as CubeLogo } from './assets/littlelemonlogo1.svg';
 function Footer(){
     return (
-    <footer id = 'footer-navigation'>
-        <CubeLogo alt = 'cube logo'/>
-        <article>
+    <footer className = 'footer-navigation'>
+        <CubeLogo className = 'cube-logo' alt = 'cube logo'/>
+        <article className = 'doormat-navigation'>
             <h1>Doormat Navigation</h1>
             <a href="#hero-section">Home</a>
             <a href = "about">About</a>
@@ -12,13 +13,13 @@ function Footer(){
             <a>Order Online</a>
             <a>Log in</a>
         </article>
-        <article>
+        <article className='contact-info'>
             <h1>Contact info</h1>
             <p>Address</p>
             <p>phone number</p>
             <p>email</p>
         </article>
-        <article>   
+        <article className='socialmedia-links'>   
             <h1>Social media links</h1>
             <a>Instagram</a>
             <a>Facebook</a>
--- src/Nav.js
+++ src/Nav.js
@@ -2,14 +2,17 @@ import './App.css';
 import { ReactComponent as Logo } from './assets/Logo.svg';
 function Nav(){
     return(
+        
     <nav className = 'topnav'>
         <Logo alt="Logo" className = 'logo'/>
         <a href="#hero-section">Home</a>
-        <a href = "about">About</a>
+        <a href = "#about">About</a>
         <a>Menu</a>
         <a>Reservations</a>
         <a>Order Online</a>
         <a>Log in</a>
+        
+        
     </nav>
     )
 }
